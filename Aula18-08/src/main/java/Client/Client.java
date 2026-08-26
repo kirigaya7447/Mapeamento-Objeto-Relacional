2745017a98b3f75ee641268842677ad30a54cfe4
kirigaya7447 <joaoh2915@gmail.com>
2026-08-25 21:57:49 -0300

    Fazendo o projeto para a próxima aula

--- Aula18-08/src/main/java/Client/Client.java
+++ Aula18-08/src/main/java/Client/Client.java
@@ -1,14 +1,23 @@
 package Client;
 
 import java.util.Scanner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import Objects.Produto;
 
 public class Client {
 	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+		EntityManager em = emf.createEntityManager();
+		
 		Scanner leia = new Scanner(System.in);
-		Produto p1 = new Produto(1, "Teste 01", 1);
-		Produto p2 = new Produto(2, "Teste 02", 2);
-		Produto p3 = new Produto(3, "Teste 03", 3);
+		Produto p1 = new Produto("Teste 01", 1);
+		Produto p2 = new Produto("Teste 02", 2);
+		Produto p3 = new Produto("Teste 03", 3);
 		
 		
 		System.out.println("Digite seu nome: ");
@@ -17,6 +26,14 @@ public class Client {
 		System.out.println(p1);
 		System.out.println(p2);
 		System.out.println(p3);
+		
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
 		
 	}
 }
--- Aula18-08/src/main/java/Objects/Produto.java
+++ Aula18-08/src/main/java/Objects/Produto.java
@@ -1,12 +1,23 @@
 package Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int cod;
+	@Column(name="nomeprod")
 	private String nome;
+	@Column(name="categprod")
 	private int categoria;
 	
-	public Produto(int cod, String nome, int categoria) {
-		this.cod = cod;
+	public Produto(String nome, int categoria) {
 		this.nome = nome;
 		this.categoria = categoria;
 	}
